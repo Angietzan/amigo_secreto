84aa3d41ff0b57cb77d5894569c2f96caa191c12
Angietzan <justinang12333@gmail.com>
2025-09-21 15:52:14 -0600

    Implementar funcion para actualizar la lista

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-let amigos = [];
+const amigos = [];
 
 function agregarAmigo(){
     let nombreamigo = document.getElementById('amigo').value;
     if (nombreamigo.length > 0){
-    amigos.push(nombreamigo);
-    document.querySelector('#amigo').value = '';
-    console.log(amigos);
-    } else {
-        alert('Por favor, inserte un nombre.')
+        if (amigos.includes(nombreamigo)){
+            alert('El nombre ya ha sido añadido.');
+            document.querySelector('#amigo').value = '';
+            }
+        else {
+            amigos.push(nombreamigo);
+            document.querySelector('#amigo').value = '';
+            console.log(amigos);
+            console.log();
+            actualizarLista();
+        }
+    } 
+    else {
+        alert('Por favor, inserte un nombre.');
     }
+}
+
+function actualizarLista() {
+  const lista = document.querySelector('ul');
+  lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        const listaAmigos = document.createElement('li'); 
+        listaAmigos.textContent = amigos[i];              
+        lista.appendChild(listaAmigos); 
+             
+            
+}
 }
